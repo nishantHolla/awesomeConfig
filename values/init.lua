92304fb9e25b01cd3e5bc19b5823d5624b9e9f5f
nishantHolla <nishantholla19@gmail.com>
2023-06-11 21:05:11 +0530

    Update values module

--- values/init.lua
+++ values/init.lua
@@ -2,11 +2,19 @@
 
 local module = {}
 
+module.awesomeDir = os.getenv('HOME') .. '/.config/awesome'
 module.terminal = os.getenv('TERMINAL') or 'contour'
 module.editor = os.getenv('EDITOR') or 'nvim'
 module.browser = os.getenv('BROWSER') or 'firefox'
 module.editorCmd = module.terminal .. ' -e ' .. module.editor
-module.layoutSuit = AwesomeWM.awful.layout.suid
+module.layoutSuit = AwesomeWM.awful.layout.suit
+module.tags = {
+	{ name = '1', key = 'a' },
+	{ name = '2', key = 's' },
+	{ name = '3', key = 'd' },
+	{ name = '4', key = 'f' },
+	{ name = '5', key = 'g' },
+}
 module.tagLayouts = {
 	module.layoutSuit.spiral,
 	module.layoutSuit.fair.horizontal,
