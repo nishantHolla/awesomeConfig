ae3966a1e05b209d0c23743499edafe1d630c872
nishantHolla <nishantholla19@gmail.com>
2023-06-11 19:56:39 +0530

    Update values module
    Add tagLayouts value

--- values/init.lua
+++ values/init.lua
@@ -6,12 +6,20 @@ module.terminal = os.getenv('TERMINAL') or 'contour'
 module.editor = os.getenv('EDITOR') or 'nvim'
 module.browser = os.getenv('BROWSER') or 'firefox'
 module.editorCmd = module.terminal .. ' -e ' .. module.editor
+module.layoutSuit = AwesomeWM.awful.layout.suid
+module.tagLayouts = {
+	module.layoutSuit.spiral,
+	module.layoutSuit.fair.horizontal,
+	module.layoutSuit.floating,
+	module.layoutSuit.max.fullscreen,
+}
 
 module.initValues = function()
 
 	terminal = module.terminal
 	editor = module.editor
 	editor_cmd = module.editorCmd
+	AwesomeWM.awful.layout.layouts = module.tagLayouts
 
 	local g = AwesomeWM.awful.screen.focused().geometry
 	module.screenWidth = g.width
