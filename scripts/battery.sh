87835bb9f7c0a4d272a11c04ac318d25cfa64d9a
nishantHolla <nishantholla19@gmail.com>
2023-07-08 17:30:14 +0530

    Fix battery script

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,17 +2,18 @@
 #!/bin/bash
 
 OUTPUT=`acpi`
-IS_CHARGING=`echo $OUTPUT | grep -c "Charging"`
-IS_NOT_CHARGING=`echo $OUTPUT | grep -c "Not charging"`
 
-if [[ "$IS_NOT_CHARGING" == "1" ]] ; then
-	VALUE=`echo "$OUTPUT" | awk '{print $5}' | sed 's/..$//'`
-else
+IS_DISCHARGING=`echo "$OUTPUT" | grep -c "Discharging"`
+IS_NOT_CHARGING=`echo "$OUTPUT" | grep -c "Not charging"`
+IS_CHARGING=`echo "$OUTPUT" | grep -c "Charging"`
+
+if [[ "$IS_CHARGING" != "0" ]] ; then
 	VALUE=`echo "$OUTPUT" | awk '{print $4}' | sed 's/..$//'`
-fi
-
-if [[ "$IS_CHARGING" == "1" ]] ; then
-	echo -en "${VALUE}C"
-else
-	echo -en "${VALUE}N"
+	echo "${VALUE}C"
+elif [[ "$IS_DISCHARGING" != "0" ]] ; then
+	VALUE=`echo $OUTPUT | awk '{print $4}' | sed 's/..$//'`
+	echo "${VALUE}N"
+elif [[ "$IS_NOT_CHARGING" != "0" ]] ; then
+	VALUE=`echo $OUTPUT | awk '{print $5}' | sed 's/.$//'`
+	echo "${VALUE}N"
 fi
